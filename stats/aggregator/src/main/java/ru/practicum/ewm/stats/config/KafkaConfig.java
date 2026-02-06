f8321ad0e0304bf6a35fc395223dbec636f6dfbe
Alexandr <aliaksandrmo@yandex.ru>
2026-02-06 11:02:50 +0300

    fix: mistakes

--- stats/aggregator/src/main/java/ru/practicum/ewm/stats/config/KafkaConfig.java
+++ stats/aggregator/src/main/java/ru/practicum/ewm/stats/config/KafkaConfig.java
@@ -9,8 +9,8 @@ import org.apache.kafka.clients.producer.Producer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
-import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+import ru.practicum.ewm.stats.avro.EventSimilarityAvro;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
 
 import java.util.Properties;
 
@@ -18,12 +18,12 @@ import java.util.Properties;
 public class KafkaConfig {
 
     @Bean
-    Consumer<String, SensorEventAvro> consumer(KafkaConfiguration consumerConfig) {
+    Consumer<String, UserActionAvro> consumer(KafkaConfiguration consumerConfig) {
         return new KafkaConsumer<>(consumerConfig.getProperties());
     }
 
     @Bean
-    Producer<String, SensorsSnapshotAvro> producer(KafkaConfiguration producerConfig) {
+    Producer<String, EventSimilarityAvro> producer(KafkaConfiguration producerConfig) {
         return new KafkaProducer<>(producerConfig.getProperties());
     }
 
